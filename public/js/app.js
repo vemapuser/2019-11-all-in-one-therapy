9c9ee89ccaba4922af340f62cce7d159da9ef7b2
TI <vemap@gmx.net>
2019-11-18 13:47:44 +0100

    moved styles to app.scss

--- public/js/app.js
+++ public/js/app.js
@@ -19287,8 +19287,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\2019-11-all-in-one-therapy\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\2019-11-all-in-one-therapy\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\xampp\htdocs\2019-11-all-in-one-therapy\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\xampp\htdocs\2019-11-all-in-one-therapy\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
