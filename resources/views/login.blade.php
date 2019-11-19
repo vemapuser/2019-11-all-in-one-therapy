ed50a5cfe6022064f004cb84db4cd4f61942d2e7
TI <vemap@gmx.net>
2019-11-19 09:00:42 +0100

    message on invalid login, bootstrap added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,9 @@
 @section('main')
     <form method="post" action="{{ route('authenticate') }}">
         @csrf
+        @if( $errors->any())
+            <p class="alert alert-danger">Die Zugangsdaten sind nicht korrekt.</p>
+        @endif
         <p><input type="text" name="name" placeholder="Benutzer"></p>
         <p><input type="password" name="password" placeholder="Passwort"></p>
         <p><button type="submit">Login</button></p>
