490c23fb4228c6e01a7a66297237babccabad632
TI <vemap@gmx.net>
2019-11-21 16:22:11 +0100

    hasPermission method on User, Role

--- app/User.php
+++ app/User.php
@@ -49,4 +49,18 @@ class User extends Authenticatable
         $role = Role::where('name', $roleName)->first();
         $this->roles()->save($role);
     }
+
+    public function hasPermission(string $permissionName): bool {
+        
+        $hasPermission = false;
+
+        foreach ($this->roles as $role) {
+            if ($role->hasPermission($permissionName)) {
+                $hasPermission = true;
+                break;
+            }
+        }
+
+        return $hasPermission;
+    }
 }   
